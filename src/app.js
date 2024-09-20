ada1e8c5bd3c7f2820a19765847cffac96e2d067
avinash0208 <avinashgupta0208@gmail.com>
2024-09-20 09:12:48 +0530

    Exploring Postman and GET POST request

--- src/app.js
+++ src/app.js
@@ -2,6 +2,21 @@ const express = require('express');
 
 const app = express();
 
+// this will only handle GET call to /user
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Avinash", lastName:"Gupta"})
+})
+
+app.post("/user",(req,res)=>{
+    console.log("Save data to DB ");
+    res.send("Data successfully saved to the DB!")
+})
+
+app.delete("/user", (req,res)=>{
+    res.send("Deleted successfully")
+})
+
+// this will match all the HTTP method API calls to /test
 app.use("/test",(req,res)=>{
     res.send("Hello from the server!");
 });
